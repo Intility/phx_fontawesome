ed09a092c86efb9549f2a98f3867140f0b2ad88b
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2022-07-21 16:34:42 +0200

    add more unit tests

--- test/component_test.exs
+++ test/component_test.exs
@@ -2,7 +2,7 @@ defmodule PhxFontawesomeFreeTest do
   use ComponentCase
   alias PhxFontawesomeFree
 
-  test "can render fontawesome solid icon correctly" do
+  test "can render fontawesome-free solid icon with current color correctly" do
     assigns = %{}
 
     html =
@@ -11,6 +11,70 @@ defmodule PhxFontawesomeFreeTest do
       """)
 
     assert html =~ "<svg class="
-    assert html =~ "Font Awesome Free"
+    assert html =~ "currentColor"
+  end
+
+  test "can render fontawesome-free solid icon with current color correctly via render component" do
+    assigns = %{}
+
+    html =
+      rendered_to_string(~H"""
+      <PhxFontawesomeFree.Solid.render icon="basketball" />
+      """)
+
+    assert html =~ "<svg class="
+    assert html =~ "currentColor"
+
+    html =
+      rendered_to_string(~H"""
+      <PhxFontawesomeFree.Solid.render icon={:basketball} />
+      """)
+
+    assert html =~ "<svg class="
+    assert html =~ "currentColor"
+  end
+
+  test "can render fontawesome-free regular icon with current color correctly" do
+    assigns = %{}
+
+    html =
+      rendered_to_string(~H"""
+      <PhxFontawesomeFree.Regular.user />
+      """)
+
+    assert html =~ "<svg class="
+    assert html =~ "currentColor"
+  end
+
+  test "can render fontawesome-free regular icon with current color correctly via render component" do
+    assigns = %{}
+
+    html =
+      rendered_to_string(~H"""
+      <PhxFontawesomeFree.Regular.render icon="user" />
+      """)
+
+    assert html =~ "<svg class="
+    assert html =~ "currentColor"
+
+    html =
+      rendered_to_string(~H"""
+      <PhxFontawesomeFree.Regular.render icon={:user} />
+      """)
+
+    assert html =~ "<svg class="
+    assert html =~ "currentColor"
+  end
+
+  test "can forward extra params to the underlying svg element" do
+    assigns = %{}
+
+    html =
+      rendered_to_string(~H"""
+      <PhxFontawesomeFree.Regular.user zoid="berg" />
+      """)
+
+    assert html =~ "zoid"
+    assert html =~ "berg"
   end
 end
